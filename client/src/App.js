071a69a56ffb35f10cd3305dacb18c2fc2a7c39a
Varinder singh <varinder83singh@gmail.com>
2019-12-04 10:23:20 -0800

    working on local

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Chat from "./components/Chat/Chat";
+import Join from "./components/Join/Join";
 
-function App() {
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const App = () => {
   return (
-    <div className="App">
-
-      We are screwed !
-    </div>
+    <Router>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
+    </Router>
   );
-}
+};
 
 export default App;
