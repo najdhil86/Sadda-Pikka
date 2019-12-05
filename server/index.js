743018691597dde2b850d96477684853477ebada
Naj Dhillon <najdhil86@gmail.com>
2019-12-05 10:22:16 -0800

    fixing the header issue

--- server/index.js
+++ server/index.js
@@ -21,6 +21,14 @@ const db = mongojs(databaseUrl, collections);
 app.use(cors());
 app.use(router);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+    next();
+});
+
+
 io.on("connect", socket => {
   socket.on("join", ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
